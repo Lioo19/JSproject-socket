3330a9b30027e071eb28cf895d40346e8a905322
Linnea <linnea.j.m.olofsson@gmail.com>
2020-10-24 11:08:53 +0200

    Fixes in math-fomula and readme

--- socket-simulation.js
+++ socket-simulation.js
@@ -6,9 +6,13 @@ let stock = {
     getPrice: function (input) {
         let start = input.startingPoint;
         let rate = 1.002;
-        let variance = 0.5;
+        let variance = 0.1;
+        let total = start * rate + variance * stock.random0();
 
-        return start * rate + variance * stock.random0();
+        if (total > 2000) {
+            return total - 1000;
+        }
+        return total;
     }
 };
 
